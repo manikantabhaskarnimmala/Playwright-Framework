6462b43b002d14aafd653b4126948f517fcb7562
Manikanta Bhaskar <bhaskar.nimmala25@gmail.com>
2026-04-24 21:05:40 +0530

    Modified package.json file for easier test execution

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,9 +30,9 @@ export default defineConfig({
       name: 'chromium',      use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',      use: { ...devices['Desktop Firefox'] },
+    // },
 
     // {
     //   name: 'webkit',      use: { ...devices['Desktop Safari'] },
